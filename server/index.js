0a19c4400f77bb744931d75f8c433e763eb42541
Яна Неронская <yana.neronskaya@gmail.com>
2018-01-31 16:50:18 +0300

    add function for send file

--- server/index.js
+++ server/index.js
@@ -6,29 +6,29 @@ let path = require('path');
 let ROOT = __dirname + "../public";
 
 http.createServer(function (req, res) {
-   if(!checkAccess(req)){
-       res.statusCode = 403;
-       res.end('Yep, sorry, bro');
-       return;
-   }
+    if (!checkAccess(req)) {
+        res.statusCode = 403;
+        res.end('Yep, sorry, bro');
+        return;
+    }
 
-   sendFileSafe(url.parse(req.url).pathname, res);
+    sendFileSafe(url.parse(req.url).pathname, res);
 }).listen(3000);
 
-function checkAccess(req){
+function checkAccess(req) {
     return url.parse(req.url, true).query.secret === 'o_O';
 }
 
-function sendFileSafe(filePath, res){
-    try{
+function sendFileSafe(filePath, res) {
+    try {
         filePath = decodeURIComponent(filePath);
-    }catch(e){
+    } catch (e) {
         res.statusCode = 400;
         res.end('Bad request');
         return;
     }
 
-    if(~filePath.indexOf('\0')){
+    if (~filePath.indexOf('\0')) {
         res.statusCode = 400;
         res.end('Bad request');
         return;
@@ -36,19 +36,29 @@ function sendFileSafe(filePath, res){
 
     filePath = path.normalize(path.join(ROOT, filePath));
 
-    if(filePath.indexOf(ROOT)!==0){
-        res.statusCode=404;
+    if (filePath.indexOf(ROOT) !== 0) {
+        res.statusCode = 404;
         res.end('File not found');
         return;
     }
 
-    fs.stat(filePath, function(err, stats){
-        if(err|| !stats.isFile()){
-            res.statusCode=404;
+    fs.stat(filePath, function (err, stats) {
+        if (err || !stats.isFile()) {
+            res.statusCode = 404;
             res.end('File not found');
             return;
         }
 
         sendFile(filePath, res);
+    });
+}
+
+function sendFile(filePath, res) {
+    fs.readerFile(filePath, function (err, content) {
+        if (err) throw err;
+
+        let mime = require('mime').lookup(filePath);
+        res.setHeader('Content-Type', mine + "; charset=utf-8");
+        res.end(content);
     });
 }
